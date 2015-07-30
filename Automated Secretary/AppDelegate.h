f3e8ca65b7852f833a76d37f9644784994bb06c2
Zachary Whitten <zachwhitten@SlashDotDash.local>
2015-07-29 20:20:50 -0400

    Initial Commit

---
+++ Automated Secretary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Automated Secretary
+//
+//  Created by Zachary Whitten on 7/29/15.
+//  Copyright (c) 2015 WCNURadio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Automated Secretary/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Automated Secretary
+//
+//  Created by Zachary Whitten on 7/29/15.
+//  Copyright (c) 2015 WCNURadio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
